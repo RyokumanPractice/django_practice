5b61a0b9c8a32f1bdb8aa65e43bc434394d5b2f6
Ryokuman <ryokuman@naver.com>
2022-07-05 15:05:15 +0900

    add topics

--- myapps/views.py
+++ myapps/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render ,HttpResponse
 
+topics = [
+    {'id':1 , 'title':'Routing' , 'body':'Routing is....'},
+    {'id':2 , 'title':'View' , 'body':'View is....'},
+    {'id':3 , 'title':'Model' , 'body':'Model is....'}
+]
+
+
 def index(request):
-    return HttpResponse("Welcome!")
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return HttpResponse(f'''
+        <html>
+            <body>
+                <h1>django</h1>
+                <ol>
+                    {ol}
+                </ol>
+                <h2>Welcome</h2>
+                Hello djnago
+            </body>
+        </html>
+    ''')
 
 def create(request):
     return HttpResponse("Create!")
